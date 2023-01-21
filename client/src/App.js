69a721fdf36c3513f7fbaf0ab330b118d7936650
jamesole <87213195+jamesole@users.noreply.github.com>
2023-01-21 11:19:37 -0500

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Search from "./components/Search";
+
+export default function App(props) {
+  return (
+    <div>
+      <Nav />
+      <Search />
+    </div>
+  );
+}
